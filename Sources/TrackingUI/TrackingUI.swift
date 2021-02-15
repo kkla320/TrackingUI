0cb120ae9baffbc22780b4bcfcdc7eddb7633240
kkla320 <accounts_kkla@icloud.com>
2021-02-15 10:10:23 +0100

    - Updated to newest analytics

--- Sources/TrackingUI/TrackingUI.swift
+++ Sources/TrackingUI/TrackingUI.swift
@@ -1,22 +1,21 @@
 import Tracking
 import SwiftUI
 
-struct AnalyticsViewModifier: ViewModifier {
+struct AnalyticsViewModifier<T>: ViewModifier where T: View {
     @Environment(\.analytics) private var analytics: Analytics
     
     var screenName: String
+    var sender: T
     
     func body(content: Content) -> some View {
         return content.onAppear {
-            analytics.log(event: "screen-appeared", parameters: [
-                "screen-name": .string(screenName)
-            ])
+            analytics.log(event: Events.screenEvent(screenName: screenName, sender: content))
         }
     }
 }
 
 extension View {
-    public func track(screenName: String) -> some View {
-        return self.modifier(AnalyticsViewModifier(screenName: screenName))
+    public func track<T>(screenName: String, sender: T) -> some View where T: View {
+        return self.modifier(AnalyticsViewModifier(screenName: screenName, sender: sender))
     }
 }
